ee5023e81dd9696a9253770f6818627e75322001
BrianTaylor <bataylor135@gmail.com>
2023-12-22 10:52:20 -0500

    first commit

---
+++ cat-fact-app/app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const axios = require('axios');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.get('/cats/fact', async (req, res) => {
+  try {
+    const response = await axios.get('https://catfact.ninja/fact');
+    const catFact = response.data.fact;
+    res.render('cat/fact', { catFact: catFact });
+  } catch (error) {
+    res.status(500).send('Error fetching cat fact');
+  }
+});
+
+require('dotenv').config();
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//port 
+
+
+
+
+module.exports = app;
